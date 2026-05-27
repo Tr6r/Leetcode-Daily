0800641fee49f7ae5a18ee78659d6620bc07e482
Dinh Cuong <tr6r20@gmail.com>
2026-05-27 23:40:57 +0700

    optimize: LC 203
    Time : O(N) - traverse every node in the head list once.
    Space : O(1)

--- esay/Remove Linked List Elements.cpp
+++ esay/Remove Linked List Elements.cpp
@@ -1,38 +1,43 @@
 /*
 LC 203 - Remove Linked List Elements.
 Time : O(N) - traverse every node in the head list once.
-Space : O(N) - create a new node in each iteration in the worst case.
+Space : O(1)
 
 Logic:
-1. Create a new linked list with a head and tail pointer.
+1. 1. Create a copy of the original linked list.
 2. traverse every node in the original list. 
-3. If the current node's value is unequal to the given value:
-   - Create a new node with that value.
-   - Append it to the new list.
-4. Move the tail pointer to the newly added node.
+3. If the current node's value is equal to the given value:
+   - if pre NULL -> move the head to the next node.
+   - else link the previous node to the next node of the current node.
+4. Move the previous pointer to the current node.
 */
 
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode *new_head = NULL;
-        ListNode *tail = NULL;
+        ListNode *new_head = head;
+        ListNode *pre = NULL;
         if (!head) return new_head;
         while ( head != NULL)
         {
-            if (head->val != val)
+            if (head->val == val)
             {
-                ListNode *new_node = new ListNode;
-                new_node->val = head->val;
-                if(new_head == NULL)
+                if(!pre)
                 {
-                    new_head = tail = new_node;
+                    if (new_head->next == NULL)
+                    {
+                        new_head = NULL;
+                        return new_head;
+                    }
+                    new_head = new_head->next;
                     head = head->next;
                     continue;
                 }
-                tail->next = new_node;
-                tail = new_node;
+                pre->next = head->next;
+                head = head->next;
+                continue;
             }
+            pre = head;
             head = head->next;
         }
         return new_head;
